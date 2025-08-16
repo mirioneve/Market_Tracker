f91d6ac3b454d62e0ddaac7cf14af8b840ba0f4a
Musbi <musbi700@gmail.com>
2025-08-16 16:54:33 +0100

    Restore urls.py

--- registrations/models.py
+++ registrations/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 class Registration(models.Model):
     """A class to manage user registration info."""
+    user_choices = {
+        "CLT" : "Client",
+        "SL": "Seller",
+    }
+
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
+    acount_type = models.CharField(max_length=80, choices=user_choices)
