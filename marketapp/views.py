938d7c54d0212a7f37af21bafe0bd5fef9d3368f
Musbi <musbi700@gmail.com>
2025-08-30 10:46:06 +0100

    removed try me file

--- marketapp/views.py
+++ marketapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "marketapp/index.html")
